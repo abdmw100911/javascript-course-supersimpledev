2a23cda887e34524b7b5cc8df22208306489196c
ABDULLAH MEWAWALA <abdmw8919@gmail.com>
2026-01-18 20:22:49 +0530

    added animations in the project

--- 9_advance functions/9_rps.js
+++ 9_advance functions/9_rps.js
@@ -1,139 +1,89 @@
-//retrieve score from local storage
-    let score = JSON.parse(localStorage.getItem('score')) || {
-        wins:0,
-        losses:0,
-        ties:0
-      };
+let score = JSON.parse(localStorage.getItem('score')) || {
+  wins: 0,
+  losses: 0,
+  ties: 0
+};
 
-    //display initial score  
-    displayScoreElement();
+displayScoreElement();
 
-    //function for resetting score
-    function resetScore(){
-      score.wins = 0;
-      score.losses = 0;
-      score.ties = 0;
+function resetScore(){
+  score = { wins: 0, losses: 0, ties: 0 };
+  localStorage.removeItem('score');
+  displayScoreElement();
+}
 
-      //deleting data from local storage
-      localStorage.removeItem('score');
+function playGame(userMove){
+  const computerMove = pickComputerMove();
+  let result = '';
 
-      //display score after reset
-      displayScoreElement();
-    }
+  if(userMove === computerMove){
+    result = 'Tie';
+    score.ties++;
+  } else if(
+    (userMove === 'Rock' && computerMove === 'Scissor') ||
+    (userMove === 'Paper' && computerMove === 'Rock') ||
+    (userMove === 'Scissor' && computerMove === 'Paper')
+  ){
+    result = 'You Win';
+    score.wins++;
+  } else {
+    result = 'You Lose';
+    score.losses++;
+  }
 
-    
-    function playGame(userMove){
+  localStorage.setItem('score', JSON.stringify(score));
 
-      //calculate result based on usermove
-      const computerMove = pickComputerMove(userMove);
-      let result='';
-      if(userMove === 'Rock'){
-        if(computerMove === 'Rock' ){
-        result='Tie';
-        }
-        else if(computerMove === 'Paper'){
-          result='You Lose';
-        }
-        else if (computerMove === 'Scissor'){
-          result='You Win';
-        }
-      }
+  displayResultElement(result);
+  displayChoiceElement(userMove, computerMove);
+  displayScoreElement();
+}
 
-      if(userMove === 'Paper'){
-        if(computerMove === 'Rock' ){
-          result='You Win';
-        }
-        else if(computerMove === 'Paper'){
-          result='Tie';
-        }
-        else if (computerMove === 'Scissor'){
-          result='You Lose';
-        }
-      }
-      if(userMove === 'Scissor'){
-        if(computerMove === 'Rock'){
-        result='You Lose';
-        }
-        else if(computerMove === 'Paper'){
-          result='You Win';
-        }
-        else if (computerMove === 'Scissor'){
-          result='Tie';
-        }
-      }
+function pickComputerMove(){
+  const r = Math.random();
+  if(r < 1/3) return 'Rock';
+  if(r < 2/3) return 'Paper';
+  return 'Scissor';
+}
 
-      //display result
-      displayResultElement(result);
+function displayScoreElement(){
+  document.querySelector('.js-score-display').innerHTML =
+    `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
+}
 
-      //update score
-      if(result === 'You Win'){
-        score.wins += 1;
-      }
-      else if(result === 'You Lose'){
-        score.losses += 1 ;
-      }
-      else if (result === 'Tie'){
-        score.ties += 1;
-      }
+function displayResultElement(result){
+  document.querySelector('.js-result-display').innerHTML = result;
+}
 
-      //storing updated score in local storage
-      localStorage.setItem('score',JSON.stringify(score));
+function displayChoiceElement(userMove, computerMove){
+  const userWins =
+    (userMove === 'Rock' && computerMove === 'Scissor') ||
+    (userMove === 'Paper' && computerMove === 'Rock') ||
+    (userMove === 'Scissor' && computerMove === 'Paper');
 
-      //display the updated score
-      displayScoreElement();
-    }
+  const tie = userMove === computerMove;
 
-    //computer picks move
-    function pickComputerMove(userMove){
+  document.querySelector('.js-choice-display').innerHTML = `
+    You
+    <img src="../projects/rps images/${userMove}.png"
+         class="choice-image ${userWins ? 'user-winner' : 'user-choice'}">
 
-      let randomNumber=Math.random();
-      let computerMove='';
+    <img src="../projects/rps images/${computerMove}.png"
+         class="choice-image ${(!userWins && !tie) ? 'computer-winner' : 'computer-choice'}">
+    Computer
+  `;
+}
 
-      if(randomNumber>=0 && randomNumber<1/3){
-        computerMove='Rock';
-      }
-      else if(randomNumber>=1/3 && randomNumber<2/3)
-      {
-        computerMove='Paper';
-      }
-      else if(randomNumber>=2/3 && randomNumber<1){
-        computerMove='Scissor'
-      }
+let isAutoPlaying = false;
+let intervalId;
 
-      //display user and computermove
-      displayChoiceElement(userMove,computerMove);
-
-      return computerMove;
-    }
-
-    //function to display score
-    function displayScoreElement(){
-      document.querySelector('.js-score-display').
-        innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`
-    }
-
-    //function to display result
-    function displayResultElement(result){
-      document.querySelector('.js-result-display').innerHTML = result;
-    }
-
-    //function to display userchoice
-    function displayChoiceElement(userMove, computerMove){
-      document.querySelector('.js-choice-display')
-        .innerHTML = `You <img src="../projects/rps images/${userMove}.png" class="choice-image"> <img src="../projects/rps images/${computerMove}.png" class="choice-image"> Computer`;
-    }
-
-    let isAutoPlaying = false;
-    let intervalId;
-    function autoPlay(){
-      if(!isAutoPlaying){
-        intervalId = setInterval(function(){
-        playGame(pickComputerMove());
-      },100);
-      isAutoPlaying = true;
-      }
-      else{
-        clearInterval(intervalId);
-        isAutoPlaying = false;
-      }
-    }
+function autoPlay(){
+  if(!isAutoPlaying){
+    intervalId = setInterval(() => {
+      playGame(pickComputerMove());
+    }, 2000);
+    isAutoPlaying = true;
+  } else {
+    clearInterval(intervalId);
+    isAutoPlaying = false;
+  }
+}
